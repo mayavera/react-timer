6afb83c77d945965f09c10f21a418363905a6d08
Maya Vera <5532165+mayavera@users.noreply.github.com>
2020-07-12 11:52:05 -0400

    Update About.tsx
    look ma, no return statement!

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,20 +1,18 @@
 import React, { FunctionComponent } from 'react';
 import './About.css';
 
-export const About: FunctionComponent = () => {
-    return (
-        <div>
-            <br />
-            <div className="jumbotron">
-                <h1 className="display-4">Timer</h1>
-                <p className="lead">Author: Brandon</p>
-                <hr className="my-4" />
-                <h5>
-                    This was a simple web app developed for the purpose of learning. <br />
-                    Created with React and TypeScript <br />
-                    Source can be found: <a href="https://github.com/merc-ury/react-timer">HERE</a>
-                </h5>
-            </div>
+export const About: FunctionComponent = () => (
+    <div>
+        <br />
+        <div className="jumbotron">
+            <h1 className="display-4">Timer</h1>
+            <p className="lead">Author: Brandon</p>
+            <hr className="my-4" />
+            <h5>
+                This was a simple web app developed for the purpose of learning. <br />
+                Created with React and TypeScript <br />
+                Source can be found: <a href="https://github.com/merc-ury/react-timer">HERE</a>
+            </h5>
         </div>
-    ); 
-};
+    </div>
+);
